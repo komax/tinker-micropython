684c2168739952e84aea42c0ba57e1eae8a790dd
Maximilian Konzack <maximilian.konzack@gmail.com>
2019-08-14 21:43:35 +0200

    Wait until connection established

--- networking.py
+++ networking.py
@@ -1,12 +1,16 @@
 import network
 
-def connect_to_wifi(essid, password=None, ifconfig=None):
+def connect_to_wifi(essid, password=None, ifconfig=None, verbose=True):
     station = network.WLAN(network.STA_IF)
     station.active(True)
-    if ifconfig and len(ifconfig) == 4:
-        station.ifconfig(ifconfig)
-    if password:
-        station.connect(essid, password)
-    else:
-        station.connect(essid)
-    print("Connected {} to: {}".format(station.isconnected(), station.ifconfig()))
+    if not station.isconnected():
+        if ifconfig and len(ifconfig) == 4:
+            station.ifconfig(ifconfig)
+        if password:
+            station.connect(essid, password)
+        else:
+            station.connect(essid)
+        while not station.isconnected():
+            pass
+    if verbose:
+        print("Connected {} to: {}".format(station.isconnected(), station.ifconfig()))
